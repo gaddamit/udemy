253a50e05c7224e7d9568c731a272bfe712b2a89
Leo Esguerra <leo.esguerra@gmail.com>
2024-08-27 21:49:56 -0600

    Test char types

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 using namespace std;
-int test;
-int main()
-{
-    int age;
-    cout << age << " " << test << endl;
+int main() {
+    char name[50];
+    wchar_t wname[50];
+    char16_t c16name[50];
+    char32_t c32name[50];
+    cout << sizeof(c32name) << endl;
     return 0;
 }
