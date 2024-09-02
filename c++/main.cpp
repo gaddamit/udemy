db24035a722ec3664a8cec79cbc0d5aeb3e50a3b
Leo Esguerra <leo.esguerra@gmail.com>
2024-09-01 18:12:48 -0600

    Refactor

--- c++/main.cpp
+++ c++/main.cpp
@@ -6,9 +6,7 @@ using namespace std;
 int main()
 {
     string input;
-    //input = "ABCD";
     getline(cin, input);
-    cout << input << endl;
 
     size_t len = input.length();
 
@@ -16,10 +14,10 @@ int main()
     for(size_t i = 0; i <= len; i++)
     {
         string temp = input.substr(0, i);
-
         string rev(mirror.rbegin(), mirror.rend());
         string space((len - i), ' ');
         cout << space << temp << rev << endl;
+
         mirror = temp;
    }
     return 0;
