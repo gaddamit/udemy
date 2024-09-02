f3f45aa804fd21bfe76e45caede568679df8612c
Leo Esguerra <leo.esguerra@gmail.com>
2024-09-02 01:52:52 -0600

    Passing array as function parameters

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,15 +2,20 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-void print(int apples = 10);
+void zero_array(int numberes[])
+{
+    for(size_t i = 0; i < 5; i++)
+    {
+        numberes[i] = 0;
+    }
+}
 int main()
 {
-    print(5);
+    int my_numbers[5] = {1, 2, 3, 4, 5};
+    zero_array(my_numbers);
+    for(size_t i = 0; i < 5; i++)
+    {
+        std::cout << my_numbers[i] << std::endl;
+    }
     return 0;
-}
-
-void print(int apples)
-{
-    cout << "I have " << apples << " apples." << endl;
 }
